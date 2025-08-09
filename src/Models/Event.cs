a8e1d18fccecbbfb1436973b7cfb694f19ef2735
Paul Houlston <paul.houlston@hotmail.co.uk>
2025-08-09 05:58:25 +0100

    Added stream ID to allow one to seggregate events to the lifetime of an entity

--- src/Models/Event.cs
+++ src/Models/Event.cs
@@ -5,6 +5,7 @@ namespace EventSourcingDemo.Models
     public class Event
     {
         public int Id { get; set; }
+        public string StreamId { get; set; } = string.Empty; // Added to associate event with a stream/entity
         public string Name { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; }
     }
